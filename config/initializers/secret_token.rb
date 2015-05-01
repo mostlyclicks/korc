392b9f4890b22ea56853a59f72a815e28c62dfea
Carlos Interone <carlos@mostlyclicks.com>
2015-05-01 08:57:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Korc::Application.config.secret_token = '6ca27678e394ac4a08c3b795a4d37f670afc56a6548f3f6e8a54a1d02480a8b2b1c3fcc57d63971a211be43700ae78242ba4c8f47ea3fa0be6dc1dc6bac6ca70'
